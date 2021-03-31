74badf92a65e558f7df576107cc9b7c88de2af2e
Crafter98 <ghkdrn4321@naver.com>
2021-03-31 22:59:42 +0900

    gridview

---
+++ app/src/main/java/com/example/streetcat/CatInfo.kt
@@ -0,0 +1,21 @@
+package com.example.streetcat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.GridView
+
+class CatInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cat_main)
+
+       /* var item = arrayOf("사진1", "사진2", "사진3", "사진4", "사진5", "사진6", "사진7", "사진8", "사진9", "사진10", "사진11", "사진12") // 임시용
+        var list : GridView = findViewById(R.id.list)
+        var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, item)
+        list.setAdapter(adapter)*/
+
+        var gridView : GridView = findViewById(R.id.gridView)
+        gridView.adapter = ImageAdapter(this)
+    }
+}
