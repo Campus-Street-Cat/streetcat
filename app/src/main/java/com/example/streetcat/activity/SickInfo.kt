0cb8dc60e7e1d4d6942835d13150ab9224942e5c
suhwan <soo7652@naver.com>
2021-05-26 20:15:26 +0900

    tmp

--- app/src/main/java/com/example/streetcat/activity/SickInfo.kt
+++ app/src/main/java/com/example/streetcat/activity/SickInfo.kt
@@ -1,14 +1,37 @@
 package com.example.streetcat.activity
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.streetcat.R
+import com.example.streetcat.adapter.SickListAdapter
+import com.example.streetcat.data.SickList
+import kotlinx.android.synthetic.main.activity_sick_info.*
 
 
 class SickInfo : AppCompatActivity() {
+    lateinit var sickAdapter : SickListAdapter
+    lateinit var sickName : String
+    private val sickLists : ArrayList<SickList> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sick_info)
 
+        sickLists.add(SickList("구토를 한다", "몇차례 심하게 토했다"))
+        sickLists.add(SickList("설사를 한다", "심한 설사를 한다"))
+        sickLists.add(SickList("혈변을 한다", "변에 피가 묻어 나온다"))
+        sickLists.add(SickList("자꾸 귀를 문지른다", "귀 뒤쪽에 빨갛고 검은 귀지가 보인다"))
+        sickLists.add(SickList("자꾸 몸을 긁는다", "몸에 빨갛게 올라온 염증이 보인다"))
+
+
+        sick_recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        sickAdapter = SickListAdapter(sickLists)
+        sick_recyclerView.adapter = sickAdapter
+        Log.d("DS","되긴하나..")
+
     }
+
 }
---
+++ app/src/main/java/com/example/streetcat/data/SickList.kt
@@ -0,0 +1,5 @@
+package com.example.streetcat.data
+
+data class SickList(val sickname : String, val sickex : String){
+    //sickname : 이상증상 이름, sickex : 아래에 적히는 이상증상 사례
+}
